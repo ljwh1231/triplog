e0b0df33230aeeafe289f7a904985a4d5d6e9d61
ljwh1231 <ljwh123161@gmail.com>
2024-03-25 02:50:42 +0900

    feat: add swagger

--- apps/server/src/user/user.controller.ts
+++ apps/server/src/user/user.controller.ts
@@ -1,12 +1,35 @@
 import { Body, Controller, Post, Get, Req } from '@nestjs/common';
 import { Supabase } from '../common/supabase/supabase';
-import { access } from 'fs';
+import {
+  ApiAcceptedResponse,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiDefaultResponse,
+  ApiFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { ResOAuth } from 'types';
 
 @Controller('users')
+@ApiTags('유저 API')
 export class UserController {
   constructor(private readonly supabaseService: Supabase) {}
 
   @Post('/')
+  @ApiOperation({
+    summary: '회원가입',
+    description: '카카오 혹은 애플 회원가입',
+  })
+  @ApiBody({
+    schema: {
+      properties: {
+        provider: { type: 'string', description: 'apple or kakao' },
+      },
+    },
+  })
+  @ApiCreatedResponse({ type: ResOAuth })
   async createUser(@Body('provider') provider: 'apple' | 'kakao') {
     const supabase = this.supabaseService.getClient();
     return await supabase.auth.signInWithOAuth({
@@ -15,6 +38,10 @@ export class UserController {
   }
 
   @Get('/')
+  @ApiOperation({
+    summary: '로그인 및 회원 정보 가져오기',
+  })
+  @ApiOkResponse()
   async signInUser(@Req() request: Request) {
     const supabase = this.supabaseService.getClient();
     const accessToken = request.headers['authorization'] as string;
