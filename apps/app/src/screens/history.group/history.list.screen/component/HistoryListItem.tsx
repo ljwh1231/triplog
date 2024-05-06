30d1d880cf6716392af1a620dc10b45ed1c40043
heojeongbo <heojungbo@gmail.com>
2024-05-07 00:55:48 +0900

    feat: change screen param

--- apps/app/src/screens/history.group/history.list.screen/component/HistoryListItem.tsx
+++ apps/app/src/screens/history.group/history.list.screen/component/HistoryListItem.tsx
@@ -1,5 +1,13 @@
+import Font from '@components/Font/Font';
+import { useNavigationService } from '@hooks/navigation';
 import { MapType } from '@repo/global-type';
-import { View } from 'react-native';
+import {
+  Pressable,
+  StyleSheet,
+  TouchableHighlight,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 type HistoryListItemProps = {
   width: number;
@@ -11,14 +19,48 @@ const tempRatio = 220 / 160;
 const HistoryListItem = (props: HistoryListItemProps) => {
   const { width, map } = props;
 
+  const { navigate } = useNavigationService();
+
+  const handlePress = () => {
+    return navigate('MapMyListScreen', { id: map.id });
+  };
+
   return (
-    <View
-      style={{
-        width,
-        height: width * tempRatio,
-        backgroundColor: 'yellow',
-      }}></View>
+    <TouchableOpacity onPress={handlePress}>
+      <View
+        style={{
+          width,
+          height: width * tempRatio,
+          ...styles.container,
+        }}>
+        <View style={styles.textContainer}>
+          <Font type="semibold_16" color="black" text={map.name} />
+          <Font
+            type="medium_14"
+            color="black"
+            text={map.createdAt.toString()}
+          />
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: 'black',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+  },
+  textContainer: {
+    padding: 20,
+    gap: 4,
+    flexDirection: 'column',
+  },
+});
 
 export default HistoryListItem;
